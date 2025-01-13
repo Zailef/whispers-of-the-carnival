dadadeee6b8ed7fa540ac098cb76c7c14cf3671a
Tom Phillips <zailef.dev@hotmail.com>
2025-01-13 01:57:11 +0000

    Add a more sensible mechanism for playing the welcome (#2)

--- scenes/characters/mushroom_folk/mushrooms_circle.gd
+++ scenes/characters/mushroom_folk/mushrooms_circle.gd
@@ -4,12 +4,17 @@ class_name MushroomCircle
 @onready var mushroom_watch_area: Area3D = $MushroomWatchArea
 @onready var mushrooms: Node3D = $Mushrooms
 @onready var audio_players: Node3D = $AudioPlayers
+@onready var player: Player = get_tree().get_first_node_in_group("player")
 
 func _ready() -> void:
 	mushroom_watch_area.body_entered.connect(_on_mushroom_watch_area_body_entered)
 	mushroom_watch_area.body_exited.connect(_on_mushroom_watch_area_body_exited)
 
-	SignalManager.player_ready.connect(_on_player_ready)
+	if not player.is_node_ready():
+		await player.ready
+
+	play_welcome()
+	set_player()
 
 	set_process(false)
 	set_physics_process(false)
@@ -24,7 +29,11 @@ func _on_mushroom_watch_area_body_exited(body: Node) -> void:
 		for mushroom in mushrooms.get_children():
 			mushroom.stop_tracking_player()
 
-func _on_player_ready() -> void:
+func set_player() -> void:
+	for mushroom in mushrooms.get_children():
+		mushroom.player = player
+
+func play_welcome() -> void:
 	for audio_player in audio_players.get_children():
 		audio_player = audio_player as AudioStreamPlayer3D
 		audio_player.play()
--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -5,7 +5,6 @@ const MAJOR_ARCANA_CARD_COUNT: int = 22
 
 @export var player_rotation_speed: float = 160.0
 
-@onready var ready_timer: Timer = $ReadyTimer
 @onready var state_machine: PlayerStateMachine = $PlayerStateMachine
 @onready var player_idle_state: PlayerIdleState = $PlayerStateMachine/PlayerIdleState
 @onready var player_move_state: PlayerMoveState = $PlayerStateMachine/PlayerMoveState
@@ -24,8 +23,6 @@ func _ready() -> void:
 	SignalManager.player_freeze_requested.connect(_on_player_freeze_requested)
 	SignalManager.player_unfreeze_requested.connect(_on_player_unfreeze_requested)
 	SignalManager.major_arcana_card_collected.connect(_on_major_arcana_card_collected)
-
-	ready_timer.timeout.connect(_on_ready_timer_timeout)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -47,10 +44,6 @@ func _on_player_freeze_requested() -> void:
 func _on_player_unfreeze_requested() -> void:
 	third_person_camera.make_current()
 	state_machine.change_state(player_idle_state)
-
-func _on_ready_timer_timeout() -> void:
-	# Slightly hacky workaround for web builds skipping audio playback due to loading times
-	SignalManager.player_ready.emit()
 
 func _on_major_arcana_card_collected(card_name: String) -> void:
 	collected_cards.append(card_name)
